b3ff39fa711f8f1a04a719206b3343db39f012ec
Lev <lev.barsegyan@devit-team.com>
2021-11-20 06:04:24 +0000

    Merged origin/master -> origin/testing

--- server.js
+++ server.js
@@ -5,6 +5,34 @@ dotenv.config()
 const app = express()
 app.use(express.urlencoded({ extended: true, }))
 app.use(express.json())
+import swaggerUi from 'swagger-ui-express'
+import yamljs from 'yamljs'
+import resolve from 'json-refs'
+import path from 'path'
+const multiFileSwagger = (root) => {
+    const options = {
+        filter: [ 'relative', 'remote' ],
+        loaderOptions: {
+            processContent: function (res, callback) {
+                callback(null, yamljs.parse(res.text))
+            },
+        },
+    }
+    return resolve.resolveRefs(root, options).then(
+        function (results) {
+            return results.resolved
+        },
+        function (err) {
+            console.log(err.stack)
+        }
+    )
+};
+(async ()=>{
+    const swaggerDocument = await multiFileSwagger(
+        yamljs.load(path.resolve(process.cwd(), './api-docs/index.yaml'))
+    )
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+})()
 initRoutes(app)
 import connection from './app/database/connection.js'
 (async () => {
