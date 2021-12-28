410ad2deee9ad98351b48c83d5f8567d67358279
Lev <lev.barsegyan@devit-team.com>
2021-12-28 18:40:33 +0000

    added production guides

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 import authController from '../app/http/controllers/authContoller.js'
 import githubSigninController from '../app/http/controllers/githubSigninController.js'
 const initRoutes = (app) => {
+    app.get('/', (req, res) => res.render('<h1>Welcome to EasyCollab</h1>'))
     app.post('/api/register', authController().register)
     app.post('/api/login', authController().login)
     app.get('/api/githubAuth', githubSigninController().githubAuth)
