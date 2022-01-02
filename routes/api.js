4a91b717ab91bec6d9ae5cd216c13dfdac3897ac
Lev <lev.barsegyan@devit-team.com>
2022-01-02 07:25:32 +0000

    fix send for now

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 import authController from '../app/http/controllers/authContoller.js'
 import githubSigninController from '../app/http/controllers/githubSigninController.js'
 const initRoutes = (app) => {
-    app.get('/', (req, res) => res.return('<h1>Welcome to EasyCollab</h1>'))
+    app.get('/', (req, res) => res.send('<h1>Welcome to EasyCollab</h1>'))
     app.post('/api/register', authController().register)
     app.post('/api/login', authController().login)
     app.get('/api/githubAuth', githubSigninController().githubAuth)
