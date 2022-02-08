3eeaa7674dc7d73378c430e41792095c7908731e
Lev <lev.barsegyan@devit-team.com>
2022-02-08 08:50:15 +0000

    review changes

--- app/http/validators/registerValidator.js
+++ app/http/validators/registerValidator.js
@@ -3,6 +3,6 @@ const RegistervalidateScheama = Joi.object({
     fullname: Joi.string().required().error(()=> Error('fullname is required!!!')),
     email: Joi.string().email().required().error(() => Error('valid email is required!!!')),
     password: Joi.string().min(8).required().error(() => Error('password should be minimum 8 char long!!!')),
-    confirmpassword: Joi.valid(Joi.ref('password')).error(() => Error('passwords should match!!!')),
+    confirmpassword: Joi.valid(Joi.ref('password')).error(() => Error('New password and confirm password do not match!')),
 })
 export default RegistervalidateScheama
