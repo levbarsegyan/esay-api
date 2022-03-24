072b0e6167865652c1e131366c2dceeb53e20e81
Lev <lev.barsegyan@devit-team.com>
2022-03-24 05:29:23 +0000

    fix oauth slash

--- routes/api.js
+++ routes/api.js
@@ -5,8 +5,11 @@ const initRoutes = (app) => {
     app.get('/', (req, res) => res.send('<h1>Welcome to EasyCollab</h1>'));
     app.post('/register', authController().register);
     app.post('/login', authController().login);
-    app.get('/oauth/redirect/github', githubSigninController().githubAuthRedirect);
-    app.post('oauth/signin/github', githubSigninController().githubAuth);
+    app.get(
+        '/oauth/redirect/github',
+        githubSigninController().githubAuthRedirect
+    );
+    app.post('/oauth/signin/github', githubSigninController().githubAuth);
     app.get('/oauth/redirect/google', GoogleAuthController().googleRedirect);
     app.post('/oauth/signin/google', GoogleAuthController().googleauth);
 };
