69ceefe1170afb76a8592aaebca76e0a37055e93
Lev <lev.barsegyan@devit-team.com>
2022-03-29 06:15:23 +0000

    fixed oauth github

--- app/http/controllers/githubSigninController.js
+++ app/http/controllers/githubSigninController.js
@@ -1,49 +1,53 @@
-import got from 'got'
-import UserModel from '../../database/models/User.js'
-import jwt from 'jsonwebtoken'
-import urls from '../../../config/urls.js'
+import got from 'got';
+import UserModel from '../../database/models/User.js';
+import jwt from 'jsonwebtoken';
+import urls from '../../../config/urls.js';
 const githubSigninController = () => {
     return {
         async githubAuthRedirect (req, res) {
-            res.redirect(urls.githubAuth.GithubAuthScreenUrl)
+            res.redirect(urls.githubAuth.GithubAuthScreenUrl);
         },
         async githubAuth (req, res) {
-            const code = req.body
-            console.log(code)
-            const response = await got.post(`${urls.githubAuth.getTokenUrl}&code=${code}`, { headers: { accept: 'application/json', }, })
-            const accessToken = JSON.parse(response.body).access_token
-            let getuser = await got.get(urls.githubAuth.getUserinfoUrl, {
-                headers: {
-                    Authorization: `token ${accessToken}`,
-                },
-            })
-            getuser = JSON.parse(getuser.body)
-            let getuserEmail = await got.get(urls.githubAuth.getUserEmailUrl, {
-                headers: {
-                    Authorization: `token ${accessToken}`,
-                },
-            })
-            getuserEmail = JSON.parse(getuserEmail.body)
-            const username = getuser.name
-            const userid = getuser.id
-            const email = getuserEmail[1].email
+            const { code, } = req.body;
+            console.log(code);
             try {
-                const isUserExist = await UserModel.findAll({ where: { userid: userid.toString(), }, })
-                if (isUserExist.length == 0) {
-                    await UserModel.create({
-                        fullname: username,
-                        email,
-                        userid,
-                        provider: 'github auth',
-                    })
+                const response = await got.post(`${urls.githubAuth.getTokenUrl}&code=${code}`, { headers: { accept: 'application/json', }, });
+                const accessToken = JSON.parse(response.body).access_token;
+                let getuser = await got.get(urls.githubAuth.getUserinfoUrl, {
+                    headers: {
+                        Authorization: `token ${accessToken}`,
+                    },
+                });
+                getuser = JSON.parse(getuser.body);
+                let getuserEmail = await got.get(urls.githubAuth.getUserEmailUrl, {
+                    headers: {
+                        Authorization: `token ${accessToken}`,
+                    },
+                });
+                getuserEmail = JSON.parse(getuserEmail.body);
+                const username = getuser.name;
+                const userid = getuser.id;
+                const email = getuserEmail[1].email;
+                try {
+                    const isUserExist = await UserModel.findAll({ where: { userid: userid.toString(), }, });
+                    if (isUserExist.length == 0) {
+                        await UserModel.create({
+                            fullname: username,
+                            email,
+                            userid,
+                            provider: 'github auth',
+                        });
+                    }
+                    const token = jwt.sign({ email, }, process.env.tokensecret, { expiresIn: '1H', });
+                    return res.status(200).json({ msg: 'sign in successfully', token: token, });
+                } catch (error) {
+                    console.log(error);
+                    return res.status(500).json({ msg: 'something went wrong', });
                 }
-                const token = jwt.sign({ email, }, process.env.tokensecret, { expiresIn: '1H', })
-                return res.status(200).json({ msg: 'sign in successfully', token: token, })
             } catch (error) {
-                console.log(error)
-                return res.status(500).json({ msg: 'something went wrong', })
+                res.status(401).json({ err: 'invelid code!!!!', });
             }
         },
-    }
-}
-export default githubSigninController
+    };
+};
+export default githubSigninController;
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,16 @@
-import authController from '../app/http/controllers/authContoller.js'
-import githubSigninController from '../app/http/controllers/githubSigninController.js'
-import GoogleAuthController from '../app/http/controllers/GoogleAuthController.js'
+import authController from '../app/http/controllers/authContoller.js';
+import githubSigninController from '../app/http/controllers/githubSigninController.js';
+import GoogleAuthController from '../app/http/controllers/GoogleAuthController.js';
 const initRoutes = (app) => {
-    app.get('/', (req, res) => res.send('<h1>Welcome to EasyCollab</h1>'))
-    app.post('/register', authController().register)
-    app.post('/login', authController().login)
-    app.get('/oauth/redirect/github', githubSigninController().githubAuthRedirect)
-    app.get('/oauth/signin/github', githubSigninController().githubAuth)
-    app.get('/oauth/redirect/google', GoogleAuthController().googleRedirect)
-    app.get('/oauth/signin/google', GoogleAuthController().googleauth)
-}
-export default initRoutes
+    app.get('/', (req, res) => res.send('<h1>Welcome to EasyCollab</h1>'));
+    app.post('/register', authController().register);
+    app.post('/login', authController().login);
+    app.get(
+        '/oauth/redirect/github',
+        githubSigninController().githubAuthRedirect
+    );
+    app.post('/oauth/signin/github', githubSigninController().githubAuth);
+    app.get('/oauth/redirect/google', GoogleAuthController().googleRedirect);
+    app.post('/oauth/signin/google', GoogleAuthController().googleauth);
+};
+export default initRoutes;
