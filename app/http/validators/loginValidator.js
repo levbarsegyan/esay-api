fdc7ee00a3b94385e193f863f2b74f3cb4cfe673
Lev <lev.barsegyan@devit-team.com>
2022-07-11 20:15:00 +0000

    fix/remember cookie

--- app/http/validators/loginValidator.js
+++ app/http/validators/loginValidator.js
@@ -2,5 +2,6 @@ import Joi from 'joi';
 const LoginvalidateScheama = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().required(),
+    remember: Joi.boolean().required(),
 });
 export default LoginvalidateScheama;
