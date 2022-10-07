dc7d8dd6e74f225dbee38e5b87bb16f69dc22d22
Lev <lev.barsegyan@devit-team.com>
2022-10-07 10:42:13 +0000

    fix(merge-conflict): merge master branch

--- __tests__/service/oauthService.test.js
+++ __tests__/service/oauthService.test.js
@@ -12,8 +12,14 @@ describe('OAuth Service Tests', function () {
         done();
     });
     it('should register user using GitHubAuth', async (done) => {
+        const env = {
+            GOOGLE_CLIENT_ID: 'GOOGLE_CLIENT_ID',
+            GOOGLE_CLIENT_SECRET: 'GOOGLE_CLIENT_SECRET',
+            GITHUB_CLIENT: 'GITHUB_CLIENT',
+            GITHUB_TOKEN: 'GITHUB_TOKEN',
+        };
         const code = 'XXXXXX';
-        const oauthService = new OAuthService();
+        const oauthService = new OAuthService({ env, });
         const data = await oauthService.GitHubAuth(code);
         expect(data).toBeDefined();
         expect(data).toHaveProperty('fullname');
@@ -24,8 +30,14 @@ describe('OAuth Service Tests', function () {
         done();
     });
     it('should register user using GoogleAuth', async (done) => {
+        const env = {
+            GOOGLE_CLIENT_ID: 'GOOGLE_CLIENT_ID',
+            GOOGLE_CLIENT_SECRET: 'GOOGLE_CLIENT_SECRET',
+            GITHUB_CLIENT: 'GITHUB_CLIENT',
+            GITHUB_TOKEN: 'GITHUB_TOKEN',
+        };
         const code = 'XXXXXX';
-        const oauthService = new OAuthService();
+        const oauthService = new OAuthService({ env, });
         const data = await oauthService.GoogleAuth(code);
         expect(data).toBeDefined();
         expect(data).toHaveProperty('fullname');
